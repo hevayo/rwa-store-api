8dbb8bd52716a57e005016b06104d680b0f880bd
Joseph Fonseka <hevayo@gmail.com>
2021-05-04 08:37:01 +0530

    Implement products api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 app.get('/products', (req, res) => {
-  res.send('Hello World!')
+  try {
+    const data = fs.readFileSync('products.json', 'utf8')
+    res.setHeader("Content-Type", "application/json; charset=UTF-8")
+    res.send(data)
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
+
+app.listen(port, function () {
+    console.log('Listening on port 3000...')
+})
